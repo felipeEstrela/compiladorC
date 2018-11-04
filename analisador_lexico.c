de289287f24c695104d38304d50e38da689c46ec
Leonardo Brandao <leolrtbrandao@gmail.com>
2018-11-04 14:10:57 -0200

    adicionando lista ligada

--- analisador_lexico.c
+++ analisador_lexico.c
@@ -46,6 +46,113 @@ Gustavo Getulio - 31686508
 #include <string.h>
 #include <ctype.h>
 
+typedef struct token
+{
+    int linha;
+    int codigo;
+} token_t;
+
+typedef struct node
+{
+    token_t *token;
+    struct node *next;
+} node_t;
+
+void print_list(node_t * head) {
+    node_t * current = head;
+
+    while (current != NULL) {
+        printf("Codigo: %d, Linha: %d\n", current->token->codigo, current->token->linha);
+        current = current->next;
+    }
+}
+
+void push_end(node_t * head, token_t * val) {
+    node_t * current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    /* now we can add a new variable */
+    current->next = malloc(sizeof(node_t));
+    current->next->token = val;
+    current->next->next = NULL;
+}
+
+void push_start(node_t ** head, token_t *val) {
+    node_t * new_node;
+    new_node = malloc(sizeof(node_t));
+
+    new_node->token = val;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int pop(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
+
+    if (*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->token->codigo;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
+
+int remove_last(node_t * head) {
+    int retval = 0;
+    /* if there is only one item in the list, remove it */
+    if (head->next == NULL) {
+        retval = head->token->codigo;
+        free(head);
+        return retval;
+    }
+
+    /* get to the second to last node in the list */
+    node_t * current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    /* now current points to the second to last item of the list, so let's remove current->next */
+    retval = current->next->token->codigo;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+
+}
+
+int remove_by_index(node_t ** head, int n) {
+    int i = 0;
+    int retval = -1;
+    node_t * current = *head;
+    node_t * temp_node = NULL;
+
+    if (n == 0) {
+        return pop(head);
+    }
+
+    for (i = 0; i < n-1; i++) {
+        if (current->next == NULL) {
+            return -1;
+        }
+        current = current->next;
+    }
+
+    temp_node = current->next;
+    retval = temp_node->token->codigo;
+    current->next = temp_node->next;
+    free(temp_node);
+
+    return retval;
+
+}
+
 int fileLenght = 0;
 
 FILE *openFile(char *nomeArq) {
@@ -646,6 +753,9 @@ int scanner(FILE *fp, FILE *tokens) {
 int main()
 {
     FILE *input, *arq_saida;
+
+    node_t *head = NULL;
+    head = malloc(sizeof(node_t));
 
     /* Arquivo do codigo AlgC que deve ser analisado */
     input = openFile("codigo.txt");
